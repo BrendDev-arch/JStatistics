c638a7092c5af3902d1e24b57d067015d06a7f82
BrendDev-arch <brendhonluccas@icloud.com>
2025-05-08 11:48:01 -0300

    Aula sobre funções e procedimentos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git!");
+        //Classe é conjunto de objetos
+        //Nesse objetivo nesse código é entender a diferença 
+        //Entre método com tipo void e método com o tipo String.
+        Mamifero vaca = new Mamifero();
+        vaca.setNomeCientífico("Bos Taurus");
+        JOptionPane.showMessageDialog(null,"Nome Científico: "+ vaca.getNomeCientífico());
+
+        Mamifero canguru = new Mamifero();
+        canguru.setNomeCientífico("Macropus");
+        JOptionPane.showMessageDialog(null,"Nome Científico: "+ canguru.getNomeCientífico());
+
+        Mamifero peixeBoi = new Mamifero();
+        peixeBoi.setNomeCientífico("Trichechus");
+        JOptionPane.showMessageDialog(null,"Nome Científico: "+ peixeBoi.getNomeCientífico());
+
+
+
+
+
     }
 }
---
+++ src/Mamifero.java
@@ -0,0 +1,18 @@
+public class Mamifero {
+
+    private int id;
+    private String nomeCientífico;
+  
+
+    public int getID(){
+        return this.id;
+    }
+    public void setNomeCientífico(String nomeCientífico){
+        this.nomeCientífico = nomeCientífico;
+    }
+
+    public String getNomeCientífico(){
+        return this.nomeCientífico;
+    }
+
+}
