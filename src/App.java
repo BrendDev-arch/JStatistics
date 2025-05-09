7550b9c7b19687634a387ec5ef51860c4a85d388
BrendDev-arch <brendhonluccas@icloud.com>
2025-05-09 08:47:54 -0300

    Alô Jardim

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Finalizando o treinamento de lógica");
+        JOptionPane.showMessageDialog(null, "Alô, Jardim !", "Boas-vindas",JOptionPane.INFORMATION_MESSAGE);
+        String nomeDosClientes;
+        JOptionPane.show
     }
 }
