afed71a148b27ef5e64978535a367e0f12e86384
BrendDev-arch <brendhonluccas@icloud.com>
2025-05-14 11:41:50 -0300

    Finalizando

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Finalizando o treinamento de lógica");
+        String msg = "Alô Jardim";
+        byte numFerramentas = 127;
+        short agendamentosMes = 32767;
+        int idAgendamento = 2147483647;
+        long dataAgendamento = System.currentTimeMillis();
+        float precoServico = 99.90f;
+        double areaJardim = 120.80;
+        char tipoServiço = 'p';
+        String cliente = "João Silva";
+        boolean concluido = true;
+
+        JOptionPane.showMessageDialog(null, msg);
+        JOptionPane.showMessageDialog(null,"Numero de ferramentas: " + numFerramentas);
+        JOptionPane.showMessageDialog(null,"Quantidade de agendamentos no mês: " + agendamentosMes);
+        JOptionPane.showMessageDialog(null," ID " + idAgendamento);    
+        JOptionPane.showMessageDialog(null," Data atual em milisegundos: " + dataAgendamento);
+        JOptionPane.showMessageDialog(null," Preço do Serviço: " + precoServico);
+        JOptionPane.showMessageDialog(null, "Area do Jardim: " + areaJardim);
+        JOptionPane.showMessageDialog(null," Tipo de Serviço: " + (tipoServiço == 'p'? "Poda":"Corte da Grama") );
+
+        if(concluido){
+            JOptionPane.showMessageDialog(null,"Faz o pix agora: " + cliente +  " você sabia que seu nome tem: " + cliente.length() + " letras" );
+        }else{
+            JOptionPane.showMessageDialog(null," O serviço em andamento ");
+        }
     }
 }
---
+++ src/AppJ.java
@@ -0,0 +1,152 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+public class AppJ {
+
+
+
+            public static void main(String[] args) throws Exception {
+                JOptionPane.showMessageDialog(null, "Alô, Jardim !", "Boas-vindas",JOptionPane.INFORMATION_MESSAGE);
+                boolean continuar = true;
+                List<Double> areas = new ArrayList<>();
+                List<Integer> servicos = new ArrayList<>();
+                int contarArea = 1;
+                while(continuar){
+                    String nomeDosClientes;
+                    String nomeDoEndereço;  
+                    Area largure = new Area();
+                    Area Comprimento = new Area(); 
+                    nomeDosClientes = JOptionPane.showInputDialog(null,"Qual é o seu nome ?");
+                    nomeDoEndereço = JOptionPane.showInputDialog(null," Qual é o seu endereço ?");
+                    double larguraDoJardim = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual a largura do seu Jardim ? " ));
+                    double comprimentoDoJardim = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual o comprimento do seu Jardim ? " ));
+                    double areaDoJardim = largure.getLargura()*Comprimento.getComprimento();
+                    areas.add(areaDoJardim);
+
+                ////////////////////////////////////////////////////////
+                
+                if(areaDoJardim < 300){
+                    JOptionPane.showMessageDialog(null, "O jardim é pequeno !", " Jardinagem ", JOptionPane.INFORMATION_MESSAGE);
+                }else{
+                    JOptionPane.showMessageDialog(null, "O jardim é grande !", " Jardinagem ", JOptionPane.INFORMATION_MESSAGE);
+                }
+                int valorDaImplantação;
+                valorDaImplantação = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor da implantação"));
+                int valorDaManutenção;
+                valorDaManutenção =  Integer.parseInt(JOptionPane.showInputDialog("Digite o valor da manutenção"));
+                int valorDoPaisagismo;
+                valorDoPaisagismo =  Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do paisagismo"));
+                
+                
+
+                    int serviço = Integer.parseInt(JOptionPane.showInputDialog(null, "Temos essas opções de serviços disponíveis no momento: " + "\n" + "1 - Implantação" + " " + valorDaImplantação + " R$" + "\n" + "2 - Manutenção" + " " + valorDaManutenção + " R$" + "\n" + "3 - Paisagismo" + " " + valorDoPaisagismo + " R$" + " \n "+ "4 - sair", JOptionPane.INFORMATION_MESSAGE));
+
+                    if(serviço != 4){
+                        switch(serviço){
+                            case 1:
+                                JOptionPane.showMessageDialog(null, "Foi escolhido o serviço de implantação", " Jardinagem ", JOptionPane.INFORMATION_MESSAGE);break;
+                            case 2:
+                                JOptionPane.showMessageDialog(null, "Foi escolhido o serviço de Manutenção", " Jardinagem ", JOptionPane.INFORMATION_MESSAGE);break;
+                            case 3:
+                                JOptionPane.showMessageDialog(null, "Foi escolhido o serviço de Paisagismo", " Jardinagem ", JOptionPane.INFORMATION_MESSAGE);break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "Opção Inválida", " Jardinagem ", JOptionPane.ERROR_MESSAGE);break;  
+                        }
+                            
+                            servicos.add(valorDaImplantação);
+                            servicos.add(valorDaManutenção);
+                            servicos.add(valorDoPaisagismo);
+
+                            servicos.sort(Integer::compareTo);
+
+                            for (Integer valor : servicos) {
+                                JOptionPane.showMessageDialog(null, "Serviço: " + valor + " R$", "Jardinagem", JOptionPane.INFORMATION_MESSAGE);
+                            }       
+
+
+                    if (!areas.isEmpty()) {
+                        double soma = 0;
+                        for (double area : areas) {
+                            soma += area;
+                        }
+                        double media = soma / areas.size();
+                        JOptionPane.showMessageDialog(null, "A média da área cadastrada é: " + media + " m²", " Jardinagem", JOptionPane.INFORMATION_MESSAGE);
+                    } 
+            
+                    else {
+                        JOptionPane.showMessageDialog(null, "Nenhuma área cadastrada para calcular a média.", " Jardinagem ", JOptionPane.ERROR_MESSAGE);
+                    }
+                    if(areaDoJardim > 100){
+                        JOptionPane.showMessageDialog(null, contarArea + " grande " ," Jardinagem " , JOptionPane.INFORMATION_MESSAGE);
+                        contarArea++;
+                    }else{
+                        JOptionPane.showMessageDialog(null, contarArea + " pequena " ," Jardinagem " , JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    double[] vetorArea = areas.stream().mapToDouble(Double::doubleValue).toArray();
+
+                    for(double area : vetorArea){
+                        JOptionPane.showMessageDialog(null, area + " foi armazenada", " Jardinagem " , JOptionPane.INFORMATION_MESSAGE);
+                    }
+                
+                    Map<Double, Integer> frequencias = new HashMap<>();
+                    for (double area : areas) {
+                        frequencias.put(area, frequencias.getOrDefault(area, 0) + 1);
+                    }
+
+                    double moda = -1;
+                    int maxFrequencia = 0;
+                    for (Map.Entry<Double, Integer> entry : frequencias.entrySet()) {
+                        if (entry.getValue() > maxFrequencia) {
+                            moda = entry.getKey();
+                            maxFrequencia = entry.getValue();
+                        }
+                    }
+                    if (moda != -1) {
+                        JOptionPane.showMessageDialog(null, moda + " é a moda encontrada", " Jardinagem ", JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Não há moda", " Jardinagem " , JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+
+
+                    int resposta = JOptionPane.showConfirmDialog(null, "Deseja escolher outra opção?", " Jardinagem ", JOptionPane.YES_NO_OPTION);
+                    if (resposta == JOptionPane.NO_OPTION) {
+                        continuar = false;   // Se a resposta for "Não", o laço termina
+                        JOptionPane.showMessageDialog(null, "Saindo do sistema...");
+                    }
+                    } else {     
+                        JOptionPane.showMessageDialog(null, "Saindo o sistema..."); 
+                        continuar = false;
+                            
+                    }
+        
+            
+        
+        }
+                   
+    }
+}
+  
+
+
+
+// try(FileWriter escreverArquivo = new FileWriter("cadastro.txt", true)){
+  //  escreverArquivo.write(" Nome do cliente: " + nomeDosClientes  + ", " + "Endereço: " + nomeDoEndereço + ", " + "A área do Jardim: " + areaDoJardim + " " + " m²" +"\n");
+    //System.out.println("dados gravados com sucesso");
+
+
+//}catch(IOException e){
+  //  System.out.println("Erro ao gravar os dados"+e.getMessage());
+
+//}
+   
