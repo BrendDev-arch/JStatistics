697fc0fa2291d902c419e9abecbc18e4ba89dccc
BrendDev-arch <brendhonluccas@icloud.com>
2025-05-15 11:56:20 -0300

    Finalizei

---
+++ src/Duvida10.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Duvida10 {
+
+    public static void main(String args[]){
+
+        double [] areasJardins = {200,300,400};
+
+        System.out.println(areasJardins[0]);
+        System.out.println(areasJardins[1]);
+        System.out.println(areasJardins[2]);
+        double soma = 0;
+        for(int i = 0; i < areasJardins.length; i++){
+            JOptionPane.showMessageDialog(null, areasJardins[i]);
+            soma+=areasJardins[i];
+        }
+        double media = soma/areasJardins.length;
+        JOptionPane.showMessageDialog(null, "A média será: " + media);
+
+          //  media = (areasJardins[0] + areasJardins[1] + areasJardins[2])/areasJardins.length;
+
+       // System.out.println("A média será: " + media);
+
+    }
+}
---
+++ src/Duvida6.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class Duvida6 {
+
+    // Criando contadores e acumuladores dentro do loops
+    // 10 fácil - programador define ...
+    // controlado por sentinela (intermediário)
+        // jardineiro - definir a quantidade
+        // Cliente - que vai comprar 
+    
+    public static final int Fidelidade = 10; // Constante
+    public static final double Desconto = 0.10;
+    
+    public static void main(String args[]){
+        int opcao;
+        double acumulador = 0;
+        int contador = 0;
+        do{
+            opcao = Integer.parseInt(JOptionPane.showInputDialog(null,"Iniciando compra: " + "\n" + " 1 - Poda (150.00) " + "\n" + "2 - Cortar Grama (250.00)" + "\n" + "3 - Sair"));  
+            if(opcao == 1){
+                acumulador+=150;
+                contador+=1;
+            }else if (opcao == 2){
+                acumulador+=250;
+                contador+=1;
+            }
+        }while(opcao != 3);
+        JOptionPane.showMessageDialog(null, "Total da compra: " + acumulador);
+        if(contador >= Fidelidade){
+            double ValorComDesconto = acumulador - (acumulador*Desconto);
+            JOptionPane.showMessageDialog(null, "Valor com desconto: " + ValorComDesconto);
+        }
+
+    }
+
+}
